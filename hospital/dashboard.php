edd426459b9bba2f4f9a38e21428c3cce5ea5b16
Tarun Kumar Dixit <tarundixit0310@gmail.com>
2020-08-07 22:41:20 +0530

    Hospital Dashboard

---
+++ hospital/dashboard.php
@@ -0,0 +1,21 @@
+<?php include "header.php" ?>
+<!-- Main content -->
+<section class="content">
+  <div class="container-fluid">
+    <div class="row">
+      <h3>This is the area to work upon!! Pickup components from the main file and drop it here!</h3>
+
+
+
+    </div>
+    <div class="row">
+      <h3>This kinda looks nice!!</h3>
+    </div>
+    <!-- /.row (main row) -->
+  </div>
+  <!-- /.container-fluid -->
+</section>
+<!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+<?php include "footer.php" ?>
